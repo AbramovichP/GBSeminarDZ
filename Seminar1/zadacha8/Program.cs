055b156c15b246400dea0f4ea5bcbfd092053449
Petr_Abramovich <malyarpetr@gmail.com>
2022-06-23 19:24:55 +0300

    added zadacha 8

---
+++ Seminar1/zadacha8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N, counter = 1;
+
+Console.WriteLine("Напишите число, а я покажу все чётные числа от 1 до этого числа: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+while(N >= counter )
+{
+    if(counter % 2 == 0)
+    {
+        Console.Write(counter + ", ");
+    }
+    counter++;
+}
+
