ae78a11283296adbbe31350f7b21aa715729a934
Petr_Abramovich <malyarpetr@gmail.com>
2022-06-30 13:12:21 +0300

    added Seminar 2 and zadacha 10

---
+++ Seminar2/zadacha10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на входе показывает цифру этого числа.
+int SecondNum(int num)
+{
+    int des = num / 10;
+    int ed = des % 10;
+    return ed;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SecondNum(number);
+Console.WriteLine("Вторая цифра введенного числа равна: "+result);
