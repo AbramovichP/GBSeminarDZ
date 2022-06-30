17fa6991f6785262607cdf774c4209d01f2064b3
Petr_Abramovich <malyarpetr@gmail.com>
2022-06-30 18:07:13 +0300

    add If in zadacha 10

--- Seminar2/zadacha10/Program.cs
+++ Seminar2/zadacha10/Program.cs
@@ -1,4 +1,4 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и на входе показывает цифру этого числа.
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на входе показывает вторую цифру этого числа.
 int SecondNum(int num)
 {
     int des = num / 10;
@@ -8,6 +8,10 @@ int SecondNum(int num)
 
 Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
-int result = SecondNum(number);
-Console.WriteLine("Вторая цифра введенного числа равна: "+result);
+if(number < 100 ^ number > 999)
+    Console.WriteLine("Было введено не трёхзначное число.");
+else
+{
+    int result = SecondNum(number);
+    Console.WriteLine("Вторая цифра введенного числа равна: "+result);
+}
