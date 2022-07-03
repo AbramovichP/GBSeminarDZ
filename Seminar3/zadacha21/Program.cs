8d6c9af029d9a9b03b20b3cfce644d8f97f015bd
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-03 13:26:32 +0300

    added zadacha 21

---
+++ Seminar3/zadacha21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double LenghtSegment3D(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double AB = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+    return AB;
+}
+
+Console.Write("Введите координату X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double distance = LenghtSegment3D(x1,y1,z1,x2,y2,z2);
+Console.WriteLine("Растояние между первой и втрой точкой в 3D пространстве равно: " + distance);
