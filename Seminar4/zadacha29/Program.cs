c85b802670c6f342473e1dc44ec609e97f7029f1
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-05 20:40:11 +0300

    added zadacha 29

---
+++ Seminar4/zadacha29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" [{array[i]}]");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = new int[8];
+
+FillArray(arr);
+PrintArray(arr);
