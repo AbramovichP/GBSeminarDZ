e21c3d24e8b54c03e15a9c705385dee0d99b642a
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-05 20:09:01 +0300

    added Seminar 4 and zadacha 27

---
+++ Seminar4/zadacha27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SummNumber(int n)
+{
+    int sum = 0;
+    int remains = 0;
+    while(n > 0)
+    {
+        remains = n % 10;
+        sum = sum + remains;
+        n = n / 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SummNumber(number);
+Console.WriteLine($"Результат суммы цифр в числе: {number}, равен: {result}");
