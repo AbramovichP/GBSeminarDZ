b67c4c3af89cfb96d9adb03d62429852a4d7308f
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-11 22:13:09 +0300

    added Seminar 5 and zadacha 34

---
+++ Seminar5/zadacha34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreatRandomArray(int size)
+{
+    int[] array = new int[size];
+    for(int i =0; i < size; i++)
+        array[i] = new Random().Next(100,1000);
+    return array;
+}
+
+int SumOfEvenNum(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length;i++)
+        if(array[i] % 2 == 0) count++;
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine("]");
+}
+
+Console.Write("Введите количество чисел в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreatRandomArray(n);
+PrintArray(arr);
+Console.WriteLine($"Количество чётных числе в массиве = {SumOfEvenNum(arr)}");
