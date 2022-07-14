625b78cf2452c0d5ac94f5284b11cd2e42fb3cef
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-14 19:27:30 +0300

    added Seminar 6 and zadacha 41

---
+++ Seminar6/zadacha41/Program.cs
@@ -0,0 +1,31 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] RequestForNumbers(int M)
+{
+    int[] array = new int[M];
+
+    for(int i = 0; i < M; i++)
+    {
+        Console.Write("Введите число № "+(i+1)+": ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int NumbersGreaterThanZero(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] > 0) count++;
+    
+    return count;
+}
+
+Console.Write("Какое количество чисел вы хотите ввести? : ");
+int volume = Convert.ToInt32(Console.ReadLine());
+if(volume > 0)
+{
+    int[] hisArray = RequestForNumbers(volume);
+    Console.WriteLine("Количество введеных чисел больше нуля = " + NumbersGreaterThanZero(hisArray));
+}
+else Console.WriteLine("На нет и суда нет.");
