7fb860ad197b026e13b5d119ca85635aec4107cd
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-26 19:15:32 +0300

    added Seminar 9 and zadacha 64

---
+++ Seminar9/zadacha64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+int DigitsNumber(int n)
+{
+    
+    if(n / 10 > 0) 
+    {
+        return DigitsNumber(n/10) + 1;  
+    }
+    else return 1;
+}
+
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество цифр в числе: " + DigitsNumber(n));
