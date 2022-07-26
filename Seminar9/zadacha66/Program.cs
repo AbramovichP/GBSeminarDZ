9b74f698f6402009e28037b291c43239deeca313
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-26 19:50:11 +0300

    added zadacha 66

---
+++ Seminar9/zadacha66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SummElements(int m, int n)
+{
+    if(n == m) 
+       return m;
+    return SummElements(m,n-1) + n;
+}
+Console.Write("Введите число М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна: {SummElements(m,n)}");
