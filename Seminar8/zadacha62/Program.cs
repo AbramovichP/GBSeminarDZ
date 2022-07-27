00b92dd758e98f3b58a2b94e6021e990cf327f93
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-27 20:38:00 +0300

    Finished zadacha 62

--- Seminar8/zadacha62/Program.cs
+++ Seminar8/zadacha62/Program.cs
@@ -1,4 +1,4 @@
-﻿// Заполните спирально массив
+﻿// Заполните спирально массива
 
 int[,] Create2dArray(int rows, int columns)
 {
@@ -20,23 +20,39 @@ void Print2dArray(int[,] array)
 
 int[,] SpiralFilling2dArray(int[,] array)
 {
-    int count = array.GetLength(0) * array.GetLength(1);
-   
-    Console.WriteLine(count);
-    for(int k = 0; k < count; k++)
-    {   
-       
-        
+            int n = array.GetLength(0);
+            int m = array.GetLength(1);
+            int row = 0;
+            int column = 0;
+            int x = 1;
+            int y = 0;
+            int dirChanges = 0;
+            int visits = m;
+ 
+            for (int i = 0; i < array.Length; i++) 
+            {
+                array[row, column] = i + 1;
+                
+              if (--visits == 0) 
+              {
+                visits = m * (dirChanges % 2) + n * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+                int temp = x;
+                x = -y;
+                y = temp;
+                dirChanges++;
+              }
+              column += x;
+              row += y;
+            }
+            return array;
 
-    }
-    return array;
 }
+
 Console.Write("Введите количество строк в массиве: ");
 int myRows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов в массиве: ");
 int myColumns = Convert.ToInt32(Console.ReadLine());
 
 int[,] myArray = Create2dArray(myRows,myColumns);
-Print2dArray(myArray);
 int[,] fillerMyArray = SpiralFilling2dArray(myArray);
 Print2dArray(fillerMyArray);
