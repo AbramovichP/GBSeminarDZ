8f34a09102ee6211f5f857c9df9cbc3f01326786
Petr_Abramovich <malyarpetr@gmail.com>
2022-07-31 11:45:31 +0300

    added zadacha 2

---
+++ Seminar10/zadacha2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив строк. 
+// Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+
+string[] PairwiseUnion(string[] array)
+{
+    string[] union = new string[array.Length];
+
+    for (int i = 0; i < array.Length - 1; i = i + 2)
+        union[i] = array[i] + array[i + 1];
+
+    return union;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+string[] words = { "qwe", "wer", "ert", "rty", "tyu", "yui"};
+string[] resultWords = PairwiseUnion(words);
+PrintArray(resultWords);
